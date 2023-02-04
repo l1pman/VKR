a8038dc1c04976fcc4a6deffb8c49aac4aa656c6
l1pman <nlipman@list.ru>
2023-02-05 03:17:58 +0700

    create edit_kcal view

--- vkr_project/balanced_diet/views.py
+++ vkr_project/balanced_diet/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import User_param_form
+from .models import User_param
 # Create your views here.
 def index(request):
     return render(request, 'balanced_diet/index.html')
@@ -24,3 +25,19 @@ def new_kcal(request):
 
     context = {'form': form}
     return render(request, 'balanced_diet/new_kcal.html', context)
+
+
+@login_required
+def edit_kcal(request):
+    user = User_param.objects.get(owner=request.user)
+    if request.method != 'POST':
+        form = User_param_form(instance=user)
+    else:
+        form = User_param_form(instance=user, data=request.POST)
+        if form.is_valid():
+            new_kcal = form.save(commit=False)
+            new_kcal.owner = request.user
+            new_kcal.save()
+            return redirect('balanced_diet:my_diet')
+    context = {'user': user, 'form': form}
+    return render(request, 'balanced_diet/edit_kcal.html', context)
