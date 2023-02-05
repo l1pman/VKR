988655547101bd722a4c390c854eb85d045c5e5c
l1pman <nlipman@list.ru>
2023-02-06 00:52:11 +0700

    create calculator in try_service.html

--- vkr_project/balanced_diet/forms.py
+++ vkr_project/balanced_diet/forms.py
@@ -7,3 +7,24 @@ class User_param_form(forms.ModelForm):
     class Meta:
         model = User_param
         fields = ['gender','weight','height','age','activity','lactose','vegan']
+
+
+class CalorieCalcForm(forms.Form):
+    GENDER_CHOICES = [
+        ('M', 'Мужской'),
+        ('F', 'Женский')
+    ]
+    ACTIVITY_CHOICES = [
+        ('1,2', 'Сидячий образ жизни.'),
+        ('1,375', 'До трех малоинтенсивных тренировок в неделю.'),
+        ('1,55', 'Тренировки три-четыре раза в неделю, тренировки интенсивные, но не тяжелые.'),
+        ('1,7',
+         'Ежедневные занятия спортом или ежедневная работа, связанная с большим количеством перемещений и ручного труда.'),
+        ('1,9', 'Экстремальная активность. Профессиональный спортсмен или работа с тяжестями и т.д.')
+    ]
+    gender = forms.ChoiceField(choices=GENDER_CHOICES)
+    weight = forms.IntegerField()
+    height = forms.IntegerField()
+    age = forms.IntegerField()
+    activity = forms.ChoiceField(choices=ACTIVITY_CHOICES)
+
--- vkr_project/balanced_diet/views.py
+++ vkr_project/balanced_diet/views.py
@@ -1,13 +1,44 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .forms import User_param_form
+from .forms import User_param_form, CalorieCalcForm
 from .models import User_param
 # Create your views here.
 def index(request):
     return render(request, 'balanced_diet/index.html')
 
 def try_service(request):
-    return render(request, 'balanced_diet/try_service.html')
+    if request.method == 'POST':
+        form = CalorieCalcForm(request.POST)
+        context = {'form': form}
+        if form.is_valid():
+
+            gender = form.cleaned_data.get('gender')
+            weight = form.cleaned_data.get('weight')
+            height = form.cleaned_data.get('height')
+            age = form.cleaned_data.get('age')
+            activity = form.cleaned_data.get('activity')
+
+            if gender == 'M':
+                kcal_value = (10 * float(weight) + 6.25 * float(height) - 5 * float(age) + 5) \
+                             * float(activity.replace(',', '.'))
+            elif gender == 'F':
+                kcal_value = (10 * float(weight) + 6.25 * float(height) - 5 * float(age) - 161) \
+                             * float(activity.replace(',', '.'))
+            proteins = kcal_value * 0.3 / 4
+            fats = kcal_value * 0.3 / 9
+            carbs = kcal_value * 0.4 / 4
+
+            context = {
+                'form': form,
+                'kcal': kcal_value,
+                'prot': proteins,
+                'fats': fats,
+                'carbs': carbs,
+            }
+            return render(request, 'balanced_diet/try_service.html', context)
+    else:
+        form = CalorieCalcForm()
+    return render(request, 'balanced_diet/try_service.html', {'form': form})
 
 @login_required
 def my_diet(request):
