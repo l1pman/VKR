9b2fe097b829c1a6c23aa646a448aca25ef475e5
l1pman <nlipman@list.ru>
2023-02-05 18:37:48 +0700

    create edit_kcal page, button

--- vkr_project/balanced_diet/urls.py
+++ vkr_project/balanced_diet/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('my_diet/', views.my_diet, name='my_diet'),
     path('new_kcal/', views.new_kcal, name='new_kcal'),
+    path('edit_kcal/', views.edit_kcal, name='edit_kcal')
 ]
