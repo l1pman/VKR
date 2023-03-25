7e2ee3dddf4d83cada2eda230dd51b7a8c7c6513
l1pman <nlipman@list.ru>
2023-03-25 16:50:51 +0700

    change labels

--- vkr_project/balanced_diet/forms.py
+++ vkr_project/balanced_diet/forms.py
@@ -7,11 +7,23 @@ class User_param_form(forms.ModelForm):
     class Meta:
         model = User_param
         fields = ['gender','weight','height','age','activity']
-
+        labels = {
+            'gender': 'Пол',
+            'weight': 'Вес',
+            'height': 'Рост',
+            'age':'Возраст',
+            'activity': 'Активность'
+        }
 class User_progress_form(forms.ModelForm):
     class Meta:
         model = User_progress
         fields = ['weight','bust','waist','hips']
+        labels = {
+            'weight':'Вес',
+            'bust':'Обхват груди',
+            'waist':'Обхват талии',
+            'hips':'Обхват бедер'
+        }
 
 class CalorieCalcForm(forms.Form):
     GENDER_CHOICES = [
@@ -26,14 +38,19 @@ class CalorieCalcForm(forms.Form):
          'Ежедневные занятия спортом или ежедневная работа, связанная с большим количеством перемещений и ручного труда.'),
         ('1,9', 'Экстремальная активность. Профессиональный спортсмен или работа с тяжестями и т.д.')
     ]
-    gender = forms.ChoiceField(choices=GENDER_CHOICES)
-    weight = forms.IntegerField()
-    height = forms.IntegerField()
-    age = forms.IntegerField()
-    activity = forms.ChoiceField(choices=ACTIVITY_CHOICES)
+    gender = forms.ChoiceField(choices=GENDER_CHOICES, label='Пол')
+    weight = forms.IntegerField(label='Вес')
+    height = forms.IntegerField(label='Рост')
+    age = forms.IntegerField(label='Возраст')
+    activity = forms.ChoiceField(choices=ACTIVITY_CHOICES, label='Активность')
 
 
 class User_prefs_form(forms.ModelForm):
     class Meta:
         model = User_prefs
         fields = ['lactose', 'vegan', 'halal']
+        labels = {
+            'lactose': 'У меня непереносимость лактозы',
+            'vegan': 'Я веган',
+            'halal': 'Я мусульманин'
+        }
